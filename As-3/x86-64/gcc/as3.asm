33cf3103c4da3b1c1733c4404a49b0d075f82a2e
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-05-03 00:05:35 +0900

    Add: as3

---
+++ As-3/x86-64/gcc/as3.asm
@@ -0,0 +1,117 @@
+partition:
+        push    rbp
+        mov     rbp, rsp
+        mov     QWORD PTR [rbp-24], rdi
+        mov     DWORD PTR [rbp-28], esi
+        mov     DWORD PTR [rbp-32], edx
+        mov     eax, DWORD PTR [rbp-32]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rbp-12], eax
+        mov     eax, DWORD PTR [rbp-28]
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-28]
+        mov     DWORD PTR [rbp-8], eax
+        jmp     .L2
+.L4:
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     eax, DWORD PTR [rax]
+        cmp     DWORD PTR [rbp-12], eax
+        jl      .L3
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rbp-16], eax
+        mov     eax, DWORD PTR [rbp-4]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     edx, DWORD PTR [rbp-8]
+        movsx   rdx, edx
+        lea     rcx, [0+rdx*4]
+        mov     rdx, QWORD PTR [rbp-24]
+        add     rdx, rcx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rdx], eax
+        mov     eax, DWORD PTR [rbp-4]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rdx, rax
+        mov     eax, DWORD PTR [rbp-16]
+        mov     DWORD PTR [rdx], eax
+        add     DWORD PTR [rbp-4], 1
+.L3:
+        add     DWORD PTR [rbp-8], 1
+.L2:
+        mov     eax, DWORD PTR [rbp-8]
+        cmp     eax, DWORD PTR [rbp-32]
+        jl      .L4
+        mov     eax, DWORD PTR [rbp-4]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     edx, DWORD PTR [rbp-32]
+        movsx   rdx, edx
+        lea     rcx, [0+rdx*4]
+        mov     rdx, QWORD PTR [rbp-24]
+        add     rdx, rcx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rdx], eax
+        mov     eax, DWORD PTR [rbp-4]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rdx, rax
+        mov     eax, DWORD PTR [rbp-12]
+        mov     DWORD PTR [rdx], eax
+        mov     eax, DWORD PTR [rbp-4]
+        pop     rbp
+        ret
+quick_sort:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 32
+        mov     QWORD PTR [rbp-24], rdi
+        mov     DWORD PTR [rbp-28], esi
+        mov     DWORD PTR [rbp-32], edx
+        mov     eax, DWORD PTR [rbp-28]
+        cmp     eax, DWORD PTR [rbp-32]
+        jge     .L8
+        mov     edx, DWORD PTR [rbp-32]
+        mov     ecx, DWORD PTR [rbp-28]
+        mov     rax, QWORD PTR [rbp-24]
+        mov     esi, ecx
+        mov     rdi, rax
+        call    partition
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-4]
+        lea     edx, [rax-1]
+        mov     ecx, DWORD PTR [rbp-28]
+        mov     rax, QWORD PTR [rbp-24]
+        mov     esi, ecx
+        mov     rdi, rax
+        call    quick_sort
+        mov     eax, DWORD PTR [rbp-4]
+        lea     ecx, [rax+1]
+        mov     edx, DWORD PTR [rbp-32]
+        mov     rax, QWORD PTR [rbp-24]
+        mov     esi, ecx
+        mov     rdi, rax
+        call    quick_sort
+.L8:
+        nop
+        leave
+        ret
